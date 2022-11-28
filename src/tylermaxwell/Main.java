9bc77477141cf1258d8ae197823765d3ed59f42a
tmax818 <tylermaxwell661@gmail.com>
2022-11-28 12:56:19 -0800

    fin

---
+++ src/tylermaxwell/Main.java
@@ -0,0 +1,13 @@
+package tylermaxwell;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Coding Dojo");
+		System.out.println("I am 100 years old");
+		System.out.println("My hometown is Burbank, CA");
+
+	}
+
+}
